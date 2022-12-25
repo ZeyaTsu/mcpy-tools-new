78ce0d48871c10852528837935808723654452a3
ZeyaTsu <43354103+ZeyaTsu@users.noreply.github.com>
2022-12-25 11:27:53 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from colorama import Fore, Back, Style
 import time
 
 import features.stronghold_finder
+import features.mineshaft_finder
+import features.chunk_blocks
 
 colorama.init()
 fc = Fore.CYAN
@@ -38,7 +40,17 @@ mcpy = """
 def menu():
     print(fb+"- - - Finder - - -"+default)
     print("1 · Stronghold Finder")
-    print("2 · Mineshaft Fidner")
+    print("2 · Mineshaft Finder")
+
+    print(fb+"- - - Vectors - - -"+default)
+    print("3 · Volume")
+    print("4 · Perimeter")
+    print("5 · Distance")
+
+    print(fb+"- - - Other - - -"+default)
+    print("6 · Chunk <=> Blocks")
+    print("7 · Y Levels for ores")
+    print("8 · CanPlace/Destroy command")
 
     choice = True
     while choice:
@@ -47,13 +59,26 @@ def menu():
             case '1':
                 features.stronghold_finder.stronghold()
                 choice = False
+            case '2':
+                features.mineshaft_finder.mineshaft()
+                choice = False
+            case '3':
+                features.volume.volume()
+            case '4':
+                features.perimeter.perimeter()
+            case '5':
+                features.distance.distance()
+            case '6':
+                features.chunk_blocks.index()
+            case '7':
+                features.levels_ore.ore()
+            case '8':
+                features.commands.canplacedestroy()
+            
                 
-
-
 def main():
     print(fc,mcpy,default)
     menu()
-
 
 if __name__ == '__main__':
     main()
