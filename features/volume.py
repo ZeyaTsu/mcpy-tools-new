e26fa87a83c2293e2f9ae79dc492d1fbea7b55a0
ZeyaTsu <43354103+ZeyaTsu@users.noreply.github.com>
2022-12-27 23:03:53 +0100

    +

--- features/volume.py
+++ features/volume.py
@@ -1,2 +1,11 @@
+import main
+import math
+import colorama
+from colorama import Fore, Back, Style
+colorama.init()
+
+instructions_color = Fore.GREEN 
+default = Fore.WHITE
+
 def volume():
-    volume()
+    print(instructions_color + "" + default)
