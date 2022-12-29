5791ff2dd8440428b72f7f293e700f63588e3944
ZeyaTsu <43354103+ZeyaTsu@users.noreply.github.com>
2022-12-29 15:52:10 +0100

    mcpymodule for discord bot

---
+++ discord/mcpymodule.py
@@ -0,0 +1,78 @@
+"""
+mcpytools-module-version
+"""
+import math
+
+def convert_chunks_blocks(block=None, chunk=None):
+    if block != None and chunk == None:
+        res = block / 16
+        msg_bot = f"{block} blocks equal to {res} chunks."
+        return msg_bot 
+    elif block == None and chunk != None:
+        resp = chunk * 16
+        msg_bot = f"{chunk} chunks equal to {resp} blocks."
+        return msg_bot
+
+def mineshaft_mirror(x_coords, z_coords):
+    x_coords = x_coords * -1
+    z_coords = z_coords * -1
+    msg_bot = f"The second mineshaft is at X: {x_coords} Z: {z_coords}"
+    return msg_bot
+
+def perimeter(x_coords, y_coords, z_coords, second_x_coords, second_y_coords, second_z_coords): 
+    x_fi = x_coords - second_x_coords
+    if x_fi < 0:
+        x_fi *= -1
+    x_fi += 1
+
+    y_fi = y_coords - second_y_coords
+    if y_fi < 0:
+        y_fi *= -1
+    y_fi += 1
+
+    z_fi = z_coords - second_z_coords
+    if z_fi < 0:
+        z_fi *= -1
+    z_fi += 1
+
+    perimeter = 2*(x_fi + z_fi) * y_fi
+    return perimeter
+
+def volume(x_coords, y_coords, z_coords, second_x_coords, second_y_coords, second_z_coords): 
+    x_fi = x_coords - second_x_coords
+    if x_fi < 0:
+        x_fi *= -1
+    x_fi += 1
+
+    y_fi = y_coords - second_y_coords
+    if y_fi < 0:
+        y_fi *= -1
+    y_fi += 1
+
+    z_fi = z_coords - second_z_coords
+    if z_fi < 0:
+        z_fi *= -1
+    z_fi += 1
+
+    volume = x_fi * z_fi * y_fi
+    return 
+
+def distance(first_x_coords, first_z_coords, second_x_coords, second_z_coords, walking_method):
+    if walking_method == "run":
+            vel = 5.6
+    if walking_method == "elyta":
+            vel = 7.2
+        
+    vector_x = second_x_coords - first_x_coords
+    if vector_x < 0:
+        vector_x *= -1
+    vector_x += 1
+
+    vector_z = second_z_coords - first_z_coords
+    if vector_z < 0:
+        vector_z *= -1
+    vector_z += 1
+
+    distance = int(math.sqrt((vector_x - 2)**2 + (vector_z -2)**2))
+    time = distance // vel
+    return time
